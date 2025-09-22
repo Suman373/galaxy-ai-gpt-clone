f3aeb5f6bbde0f054c124840a8d013c831c1d2ac
Suman Roy <iamsuman898@gmail.com>
2025-09-22 22:08:40 +0530

    fix: server comp await params

--- apps/web/app/c/[id]/page.tsx
+++ apps/web/app/c/[id]/page.tsx
@@ -1,8 +1,8 @@
 import MainChat from "@/ui/chat/MainChat";
 import { loadChatMessages } from "@/utils/chat-store";
 
-export default async function ChatPage({ params }: { params: { id: string } }) {
-    const { id: chatId } = params;
+export default async function ChatPage({ params }: {params: Promise<{id: string}>}) {
+    const { id: chatId } = await params;
     const messages = await loadChatMessages(chatId);
     return (
       <>
